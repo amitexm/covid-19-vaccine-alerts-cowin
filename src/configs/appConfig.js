bb4b7ce908bcd14637b24ee2c9cdf1d19ee3ab39
amitexm <60008947+amitexm@users.noreply.github.com>
2021-05-13 23:41:40 +0530

    Update appConfig.js

--- src/configs/appConfig.js
+++ src/configs/appConfig.js
@@ -1,6 +1,6 @@
 const FINDBYPINCODE = false;
 // location configs
-const PINCODE = '800001';
+const PINCODE = '273303';
 const STATE = 'UTTAR PRADESH';
 const DISTRICT = 'MAHARAJGANJ';
 // beneficiary configs
