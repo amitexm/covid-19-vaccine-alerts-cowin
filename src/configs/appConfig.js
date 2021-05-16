813aaddd344bdd4d1bd4113a7561c728a8e7c820
amitexm <60008947+amitexm@users.noreply.github.com>
2021-05-17 02:13:44 +0530

    Update appConfig.js

--- src/configs/appConfig.js
+++ src/configs/appConfig.js
@@ -1,8 +1,8 @@
 const FINDBYPINCODE = false;
 // location configs
-const PINCODE = '273303';
+const PINCODE = '273301';
 const STATE = 'UTTAR PRADESH';
-const DISTRICT = 'MAHARAJGANJ';
+const DISTRICT = 'GORAKHPUR';
 // beneficiary configs
 const EMAIL = 'bum@ichigo.me';
 const AGE = 28;
